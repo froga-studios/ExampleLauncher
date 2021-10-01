a1ab3f112eace1de8ca66bac6537ca19095c96f0
ExoPlant <lennyj2468+github@gmail.com>
2021-10-01 21:25:56 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let instanceName = "test";
+let instance = document.querySelector("#instance");
+function instancename() {
+    instance.innerText = instanceName;
+    console.log("Loaded Instance(s)");
+}
